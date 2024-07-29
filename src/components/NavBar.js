6aafc3a931e94fe1784f258dbaa1d43363008204
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-29 23:12:32 +0000

    add bootstrap navbar #16

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.jpg";
 
 const NavBar = () => {
   return (
-    <div>NavBar placeholder</div>
-  )
-}
+<Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              Home
+            </Nav.Link>
+            <Nav.Link>
+              Sign in
+            </Nav.Link>
+            <Nav.Link>
+              Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar
