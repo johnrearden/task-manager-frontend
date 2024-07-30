a9b7579c6d0cacb348df644249bf3da59e54a8de
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-30 18:47:51 +0000

    fetch post from API & log it to console #23

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 import appStyles from "../../App.module.css";
 
 function TaskDetail() {
-  // Add your logic here
+    const { id } = useParams();
+    const [task, setTask] = useState({ results: [] });
 
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const [{ data: task }] = await Promise.all([
+              axiosReq.get(`/tasks/${id}`),
+            ]);
+            setTask({ results: [task] });
+            console.log(task);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, [id]);
 
   return (
     <Row className="h-100">
