51169449598da05e348c8fe566c99c6bfc0988ef
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-04 23:13:09 +0000

    add ContextProvider for profileData #59
    so that profileData are accessible throughout the app

--- src/pages/profiles/ProfileList.js
+++ src/pages/profiles/ProfileList.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 import Profile from "./Profile";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
@@ -11,29 +10,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
  * Render the list of profiles from most to least recently updated
  */
 const ProfileList = () => {
-  const [profileData, setProfileData] = useState({
-    // we will use the pageProfile later!
-    pageProfile: { results: [] },
-    profileList: { results: [] },
-  });
-  const { profileList } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get("/profiles/?ordering=-updated_at");
-        setProfileData((prevState) => ({
-          ...prevState,
-          profileList: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { profileList } = useProfileData();
 
   return (
     <Container className={appStyles.Content}>
