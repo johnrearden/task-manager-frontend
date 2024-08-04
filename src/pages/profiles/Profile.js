99fd3bf89ebdc361ca39045b7cf777ad08b6d582
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-04 22:54:35 +0000

    add code comments to Profile #59

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -3,18 +3,24 @@ import styles from "../../styles/Profile.module.css";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 
+/**
+ * Renders a clickable list of users with profile pic & name
+ */
 const Profile = (props) => {
   const { profile, imageSize = 55 } = props;
   const { id, image, owner, firstname, lastname } = profile;
 
   return (
+    // whole unit of profile pic + name is clickable
     <Link className="align-self-center" to={`/profiles/${id}`}>
       <div
         className={`my-3 d-flex align-items-center`}
       >
+        {/* profile pic */}
         <div>
           <Avatar src={image} height={imageSize} />
         </div>
+        {/* profile name */}
         <div className={`mx-2 ${styles.WordBreak}`}>
           {/* show first name, last name or both if available
                   otherwise, show username */}
