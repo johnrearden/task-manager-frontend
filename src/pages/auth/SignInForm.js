7091b6a86a8a6076984ea4bc6e3433550797d26d
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-05 17:07:21 +0000

    add token timestamp to  storage on signin #78   - in `SignInForm`, `import { setTokenTimestamp } from "../../utils/utils";   - add `setTokenTimestamp(data);` to the `try` block

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -16,6 +16,7 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
@@ -36,6 +37,9 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
+      // store token timestamp in local storage
+      setTokenTimestamp(data);
+      // redirect user
       history.goBack();
         } catch (err) {
       setErrors(err.response?.data);
