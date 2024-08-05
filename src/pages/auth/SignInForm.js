dd62685fdb462b64fe0c778d4cf2228e6c2bb784
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-05 16:10:05 +0000

    redirect logged in users #76
    - create `useRedirect` hook in `hooks/`
    - use hook in `SignInForm`:
      - import `useRedirect`
      - pass `useRedirect` the prop `"loggedIn"`
      - redirect user to the home page: `history.goBack();`

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,9 +15,11 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
+  useRedirect("loggedIn");
 
   const [signInData, setSignInData] = useState({
     username: "",
@@ -34,8 +36,8 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      history.push("/");
-    } catch (err) {
+      history.goBack();
+        } catch (err) {
       setErrors(err.response?.data);
     }
   };
