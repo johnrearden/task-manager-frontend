6d7c0afcb4bb8973cd86d05aa0b55e38d55d114f
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-06 22:38:09 +0000

    remove placeholder #25

--- src/pages/tasks/TaskList.js
+++ src/pages/tasks/TaskList.js
@@ -51,7 +51,6 @@ function TaskList({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Recently updated tasks on mobile</p>
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
