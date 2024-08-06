fd7389e205c3e530c9b9d12814e4db3c6e8ede25
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-08-06 15:11:59 +0000

    add Profile List to TaskDetail page #90

--- src/pages/tasks/TaskDetail.js
+++ src/pages/tasks/TaskDetail.js
@@ -8,6 +8,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 
 // import appStyles from "../../App.module.css";
 import Task from "./Task";
+import ProfileList from "../profiles/ProfileList";
 
 function TaskDetail() {
     const { id } = useParams();
@@ -32,14 +33,13 @@ function TaskDetail() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Recently updated tasks for mobile</p>
         <Task {...task.results[0]} setTasks={setTask} taskDetail/>
         {/* <Container className={appStyles.Content}>
           Comments
         </Container> */}
       </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Recently updated tasks for desktop
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <ProfileList />
       </Col>
     </Row>
   );
